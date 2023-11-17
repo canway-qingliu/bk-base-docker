32101d1006a3bf0e13612c7a673af1581cff3cde
schuck <schuck@canway.net>
2023-11-17 12:18:03 +0800

    jobnavi

--- jobnavi/package.sh
+++ jobnavi/package.sh
@@ -7,12 +7,11 @@ result_package_path=/Users/liuqing/opt/docker/bk-base-docker/jobnavi
 
 docker run --rm --name jobnavi -e WORKSPACE="/bkdata" -e RELEASE_ENV="ee" -e BuildNo="137"  -v $base_path:/bkdata/code -v $result_package_path:/bkdata/code/result_package -v $repo_path:/root/.m2/repository -itd bk-base-common:v1 /bin/bash
 
-
 # 2.拷贝脚本并执行
 chmod +x jobnavi_package.sh
 container_id=`docker ps | grep jobnavi | awk '{print $1}'`
-docker cp /opt/bkbase_ee/bk-base/lib/settings.xml ${container_id}:/opt/maven/conf
-docker cp /opt/bkbase_ee/bin/jobnavi_package.sh ${container_id}:/bkdata
+docker cp settings.xml ${container_id}:/opt/maven/conf
+docker cp jobnavi_package.sh ${container_id}:/bkdata
 # docker exec -d ${container_id} /bin/bash -c '/bkdata/jobnavi_package.sh'
 docker exec -d ${container_id} /bin/bash -c 'nohup /bkdata/jobnavi_package.sh > /bkdata/jobnavi.log 2>&1 &'
 docker exec -it ${container_id} /bin/bash -c 'tail -f /bkdata/jobnavi.log'
