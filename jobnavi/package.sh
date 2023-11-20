3036e930805d601e7cd2c134a2b4edea04f2d848
schuck <schuck@canway.net>
2023-11-20 10:52:37 +0800

    前台启动

--- jobnavi/package.sh
+++ jobnavi/package.sh
@@ -12,7 +12,7 @@ chmod +x jobnavi_package.sh
 container_id=`docker ps | grep jobnavi | awk '{print $1}'`
 docker cp settings.xml ${container_id}:/opt/maven/conf
 docker cp jobnavi_package.sh ${container_id}:/bkdata
-# docker exec -d ${container_id} /bin/bash -c '/bkdata/jobnavi_package.sh'
-docker exec -d ${container_id} /bin/bash -c 'nohup /bkdata/jobnavi_package.sh > /bkdata/jobnavi.log 2>&1 &'
+docker exec -d ${container_id} /bin/bash -c '/bkdata/jobnavi_package.sh'
+# docker exec -d ${container_id} /bin/bash -c 'nohup /bkdata/jobnavi_package.sh > /bkdata/jobnavi.log 2>&1 &'
 docker exec -it ${container_id} /bin/bash -c 'tail -f /bkdata/jobnavi.log'
 docker stop ${container_id}
